6cfe4123aca576c50ac1385c746d32843b234eb7
georgyshkliaryk <shkliargosh23@gmail.com>
2020-12-08 01:45:36 +0100

    header added

---
+++ src/components/Layout/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    return(
+        <div className='main__container'>
+            123
+        </div>
+    )
+}
+
+export default Main;
